4efc905f9cf6cf903d9b8f83db1b50a1ac3b28ed
DhruvShah <dhruvshah78223@gmail.com>
2025-07-09 22:38:56 +0530

    latest code updated.

--- code.go
+++ code.go
@@ -1,35 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"time"
-)
+import "fmt"
 
-// fetch makes a simple GET request and sends the result to the channel
-func fetch(url string, ch chan string) {
-	start := time.Now()
-	resp, err := http.Get(url)
-	if err != nil {
-		ch <- fmt.Sprintf("Error fetching %s", url)
-		return
+func printMarks(marks ...int) {
+	for i, mark := range marks {
+		fmt.Printf("Subject %d: %d marks\n", i+1, mark)
 	}
-	defer resp.Body.Close()
-	duration := time.Since(start)
-	ch <- fmt.Sprintf("Fetched %s in %v", url, duration)
 }
 
 func main() {
-	urls := []string{"https://golang.org", "https://google.com", "https://github.com"}
-	ch := make(chan string)
-
-	// Start a goroutine for each URL
-	for _, url := range urls {
-		go fetch(url, ch)
-	}
-
-	// Receive results from the channel
-	for range urls {
-		fmt.Println(<-ch)
-	}
+	subjectMarks := []int{78, 85, 92, 88}
+	printMarks(subjectMarks...)
 }
